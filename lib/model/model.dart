b8e1cbf5a88cb57c0bada324c325ea404626fc3a
Maruf Robin <maruf.robin@northsouth.edu>
2022-11-02 22:34:09 +0600

    ModelData added.

---
+++ lib/model/model.dart
@@ -0,0 +1,11 @@
+class ModelData {
+  String productName;
+  String productImageName;
+  String productRating;
+  String distance;
+  double proudctPrice;
+  double totalPrice;
+
+  ModelData(this.productName, this.productImageName, this.proudctPrice,
+      this.totalPrice, this.productRating, this.distance);
+}
